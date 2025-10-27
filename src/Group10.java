32714e600ea33d7cedbcc7331d81907c3a5312a6
Kerem İrfanoğlu <keremirfanoglu@192.168.1.52>
2025-10-27 14:37:13 +0300

    kerem commit

--- src/Group10.java
+++ src/Group10.java
@@ -680,7 +680,7 @@ public class Group10 {
 
     }
 
-        //================KEREM IRFANOGLU================
+        //================KEREM IRFANOGLU================ ...
     //====================Secondary School====================
     public static void secondarySchool(){
 
