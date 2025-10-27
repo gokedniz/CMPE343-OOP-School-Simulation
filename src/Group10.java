aba0c7acc0b5e7f97a0161a8cdc2d4c6e02df488
keremirfanoglu <kerem.irfanoglu@stu.khas.edu.tr>
2025-10-27 15:06:55 +0300

    kerem commit 3

--- src/Group10.java
+++ src/Group10.java
@@ -680,7 +680,7 @@ public class Group10 {
 
     }
 
-        //...deneme...
+        //...deneme2
         //================KEREM IRFANOGLU================ ...
     //====================Secondary School====================
     public static void secondarySchool(){
