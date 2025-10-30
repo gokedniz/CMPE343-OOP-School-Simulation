47894b70f7d62f11b9bb94b0cab6731b8290e5ab
gulfemkomurcu <gulfemkomurcu@stu.khas.edu.tr>
2025-10-30 17:55:12 +0300

    gulfem changes in the agezodiac part

--- src/Group10.java
+++ src/Group10.java
@@ -1024,7 +1024,7 @@ public class Group10 {
     public static void PrimarySchoolMenu() {
                     boolean subMenu = true;
                     while (subMenu) {
-                        clearScreen();  //Alt menü başında ekranı temizle
+                        clearScreen();  
                         System.out.println(COLOR_LIGHT_CYAN);
                         System.out.println("╔════════════════════════════════════════════╗");
                         System.out.println("║        === Primary School Menu ===         ║");
@@ -1044,18 +1044,18 @@ public class Group10 {
                         System.out.println("║[3] Back to Main Menu                       ║");
                         System.out.println("╚════════════════════════════════════════════╝");
 
-                        System.out.print("Enter choice: "); // Bu satır çerçeve dışında kalmalı
+                        System.out.print("Enter choice: "); 
                         String Choice = input.nextLine().trim();
 
                         switch (Choice) {
                             case "1":
-                                clearScreen(); // Age and Zodiac başlamadan önce ekranı temizle
+                                clearScreen(); 
                                 ageAndZodiacDetection();
                                 System.out.println("\nPress Enter to return to Primary School Menu...");
                                 input.nextLine();
                                 break;
                             case "2":
-                                clearScreen(); // Reverse Words başlamadan önce ekranı temizle
+                                clearScreen(); 
                                 reverseTheWords();
                                 System.out.println("\nPress Enter to return to Primary School Menu...");
                                 input.nextLine();
@@ -1202,10 +1202,27 @@ public class Group10 {
             System.out.println(COLOR_RESET);
             return;
         }
+       
+        // ---------- AGE CALCULATION CONTINUED - MONTHS AND DAYS
+        int monthsPassed = currentMonth - birthMonth;
+        int daysPassed = currentDay - birthDay;
+
+        // Adjust for negative day difference
+        if (daysPassed < 0) {
+            daysPassed += 30; // We assume every moth has 30 days for simplicity
+            monthsPassed--;
+        }
+
+        // Adjust for negative month difference
+        if (monthsPassed < 0) {
+            monthsPassed += 12;
+        }
+
         System.out.println(COLOR_LIGHT_GREEN);
         System.out.println("╔═════════════╗");
-        System.out.println("║Your age is: ║  " + age + "     ");
+        System.out.println("║Your age is: ║  " + age + " years, " + monthsPassed + " months, " + daysPassed + " days.");
         System.out.println("╚═════════════╝");
+        System.out.println(COLOR_RESET);
 
         // ----------- AGE AND ZODIAC SIGN CALCULATION
         
@@ -1243,7 +1260,7 @@ public class Group10 {
         if (month == 2){
             if(day > 29)
                 return false;
-            if(day == 29 && year % 4 != 0)
+            if(day == 29 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)))
                 return false;
         }
         if (month == 3 && day > 31) {
@@ -2422,7 +2439,6 @@ public class Group10 {
                 case 2:
                     clearScreen();
                     distanceBetweenTwoArrays(choice);
-                    promptEnterToContinue();
                     break;
                 case 3:
                     System.out.println("Returning to previous menu...");
@@ -2479,7 +2495,6 @@ public class Group10 {
         }
 
         Arrays.sort(arr); // Sorting for the median
-
         // Calculations
         double median = computeMedian(arr);
         double arithmeticMean = computeArithmeticMean(arr);
@@ -2507,7 +2522,6 @@ public class Group10 {
             System.out.printf("Harmonic Mean: %s%n", formatDoubleOrMsg(harmonicMean, ""));
         }
         System.out.println(COLOR_RESET);
-        promptEnterToContinue();
     }
 
     /**
